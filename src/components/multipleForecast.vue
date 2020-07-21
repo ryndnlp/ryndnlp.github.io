6b48f3db515c06941b6574a49d2ab0ec2c6a330d
ryndnlp <ryandaniel216@gmail.com>
2020-07-21 23:31:59 +0700

    Fix icon size

--- src/components/multipleForecast.vue
+++ src/components/multipleForecast.vue
@@ -1,15 +1,15 @@
 <template>
-    <div :class="{singleForecast: isSingle, mulForecast: !isSingle}">
-        <h1 v-if="citiesName().length === 1">{{citiesName()[0]}}</h1>
-        <div :class="{singResult: isSingle, mulResult: !isSingle}" v-for="(value, index) in processedData" :key="index">
-            <h1 v-if="citiesName().length > 1">{{citiesName()[index]}}</h1>
-            <div :class="{singSegment: isSingle, mulSegment: !isSingle}" v-for="data in value.hourly" :key="data.key">
+    <div id="multipleForecast">
+        <div id="result" v-for="(value, index) in processedData" :key="index">
+            <h1>{{citiesName[index]}}</h1>
+            <div class="segment" v-for="data in value.hourly" :key="data.key">
                 <p>{{data.date | dateFilter}}</p>
                 <p>{{data.time | timeFilter}}</p>
                 <template v-for="(param, pIndex) in data.hvalue">
                     <div :key="pIndex" v-if="param.unit === '%'">
                         <img src="../assets/drop.svg" :key="pIndex" class="img">
                         <p class="icontext">{{param.value}}{{param.unit}}</p>
+                        <img v-if="param.unit === 'icon'" :src="getImgUrl(param.value)" :key="pIndex" class="icons">
                     </div>
                     <div :key="pIndex" v-else-if="param.unit === 'deg'">
                         <img src="../assets/compass.svg" :key="pIndex" class="img">
@@ -23,7 +23,7 @@
                         <p v-if="param.unit !== 'icon'" :key="pIndex" :class="{drop: param.unit==='Kt'}">
                             <span :class="{deg: param.unit==='C'}">{{param.value}}</span>{{param.unit}}
                         </p>
-                        <img v-if="param.unit === 'icon'" :src="getImgUrl(param.value)" :key="pIndex" :class="{singIcons: isSingle, mulIcons: !isSingle}">
+                        <img v-if="param.unit === 'icon'" :src="getImgUrl(param.value)" :key="pIndex" class="icons">
                     </template>
                 </template>
             </div>
@@ -39,12 +39,16 @@ export default {
     },
     data() {
         return{
+            
         }
     },
     props: [
         'processedData'
     ],
     methods:{
+        check(){
+            console.log(this.citiesName);
+        },
         getImgUrl(value) {
             const val = parseInt(value, 10);
             const images = require.context('../assets/', false, /\.svg$/);
@@ -63,9 +67,14 @@ export default {
                 else if(val === 95 || val === 97) name = 'thunderstorm';
                 else name = 'weather';
             return images('./' + name + ".svg");
-        },
+        }
+    },
+    computed: {
         citiesName: function(){
-            return this.processedData.map(cityName => cityName.city);
+            if(this.processedData.length !== 0){
+                return this.processedData.map(cityName => cityName.city);
+            }
+            else return null;
         }
     },
     filters: {
@@ -76,10 +85,7 @@ export default {
             return `${value.slice(6)}-${value.slice(4,6)}-${value.slice(0,4)}`
         }
     },
-    computed: {
-        isSingle: function(){
-            return this.citiesName().length === 1;
-        }            
+    components: {
     }
 }
 </script>
@@ -92,20 +98,15 @@ export default {
 h1{
     font-size: 5rem;
 }
-.multipleForecast, .singleForecast{
+#multipleforecast{
     margin: 0 auto;
     text-align: center;
 }
-.mulIcons{
+.icons{
     height: 150px;
     width: 150px;
 }
-.singIcons{
-    height: 200px;
-    width: 200px;
-    margin: -10px 0
-}
-.mulResult{
+#result{
     display: flex;
     flex-direction: column;
     justify-content: space-around;
@@ -116,17 +117,7 @@ h1{
     margin: 0 auto;
     align-items: center;
 }
-.singResult{
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-    padding: 2rem;
-    margin: 2rem;
-    flex-wrap: wrap;
-    max-width: 1000px;
-    margin: 0 auto;
-}
-.mulSegment{
+.segment{
     height: 200px;
     width: 800px;
     margin: 20px;
@@ -137,23 +128,13 @@ h1{
     border: 1px #f2f2f2 solid;
     box-shadow: 0 0 15px #03dac5;
 }
-.singSegment{
-    height: 400px;
-    width: 300px;
-    margin: 40px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    border: 1px #f2f2f2 solid;
-    box-shadow: 0 0 15px #03dac5;
-}
-.mulSegment > *{
+.segment > *{
     margin: 0 10px;
 }
 .icontext{
     display: inline-block;
 }
+
 .img{
     margin-right: 2px;
     width: 15px;
--- src/components/singleForecast.vue
+++ src/components/singleForecast.vue
@@ -1,15 +1,15 @@
 <template>
-    <div :class="{singleForecast: isSingle, mulForecast: !isSingle}">
-        <h1 v-if="citiesName().length === 1">{{citiesName()[0]}}</h1>
-        <div :class="{singResult: isSingle, mulResult: !isSingle}" v-for="(value, index) in processedData" :key="index">
-            <h1 v-if="citiesName().length > 1">{{citiesName()[index]}}</h1>
-            <div :class="{singSegment: isSingle, mulSegment: !isSingle}" v-for="data in value.hourly" :key="data.key">
+    <div id="singleForecast">
+        <h1>{{cityName()}}</h1>
+        <div id="result" v-for="(value, vIndex) in processedData" :key="vIndex">
+            <div class="segment" v-for="(data, dIndex) in value.hourly" :key="dIndex">
                 <p>{{data.date | dateFilter}}</p>
                 <p>{{data.time | timeFilter}}</p>
                 <template v-for="(param, pIndex) in data.hvalue">
                     <div :key="pIndex" v-if="param.unit === '%'">
                         <img src="../assets/drop.svg" :key="pIndex" class="img">
                         <p class="icontext">{{param.value}}{{param.unit}}</p>
+                        <img v-if="param.unit === 'icon'" :src="getImgUrl(param.value)" :key="pIndex" class="icons">
                     </div>
                     <div :key="pIndex" v-else-if="param.unit === 'deg'">
                         <img src="../assets/compass.svg" :key="pIndex" class="img">
@@ -23,7 +23,7 @@
                         <p v-if="param.unit !== 'icon'" :key="pIndex" :class="{drop: param.unit==='Kt'}">
                             <span :class="{deg: param.unit==='C'}">{{param.value}}</span>{{param.unit}}
                         </p>
-                        <img v-if="param.unit === 'icon'" :src="getImgUrl(param.value)" :key="pIndex" :class="{singIcons: isSingle, mulIcons: !isSingle}">
+                        <img v-if="param.unit === 'icon'" :src="getImgUrl(param.value)" :key="pIndex" class="icons">
                     </template>
                 </template>
             </div>
@@ -38,7 +38,9 @@ export default {
         document.documentElement.style.minHeight = "100vh";
     },
     data() {
-        return{
+        return {
+            ops:'data',
+            chosenType: [0, 5, 7, 8]
         }
     },
     props: [
@@ -64,8 +66,9 @@ export default {
                 else name = 'weather';
             return images('./' + name + ".svg");
         },
-        citiesName: function(){
-            return this.processedData.map(cityName => cityName.city);
+        cityName: function(){
+            if(this.processedData.length !== 0) return this.processedData[0].city;
+            else return null;
         }
     },
     filters: {
@@ -77,9 +80,7 @@ export default {
         }
     },
     computed: {
-        isSingle: function(){
-            return this.citiesName().length === 1;
-        }            
+        
     }
 }
 </script>
@@ -90,33 +91,19 @@ export default {
     text-align: center;
 }
 h1{
+    margin-top: 1rem;
     font-size: 5rem;
 }
-.multipleForecast, .singleForecast{
+#singleforecast{
     margin: 0 auto;
     text-align: center;
 }
-.mulIcons{
-    height: 150px;
-    width: 150px;
-}
-.singIcons{
+.icons{
     height: 200px;
     width: 200px;
     margin: -10px 0
 }
-.mulResult{
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    padding: 2rem;
-    margin: 2rem;
-    flex-wrap: wrap;
-    max-width: 1000px;
-    margin: 0 auto;
-    align-items: center;
-}
-.singResult{
+#result{
     display: flex;
     flex-direction: row;
     justify-content: space-around;
@@ -126,18 +113,7 @@ h1{
     max-width: 1000px;
     margin: 0 auto;
 }
-.mulSegment{
-    height: 200px;
-    width: 800px;
-    margin: 20px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    border: 1px #f2f2f2 solid;
-    box-shadow: 0 0 15px #03dac5;
-}
-.singSegment{
+.segment{
     height: 400px;
     width: 300px;
     margin: 40px;
@@ -148,12 +124,10 @@ h1{
     border: 1px #f2f2f2 solid;
     box-shadow: 0 0 15px #03dac5;
 }
-.mulSegment > *{
-    margin: 0 10px;
-}
 .icontext{
     display: inline-block;
 }
+
 .img{
     margin-right: 2px;
     width: 15px;
@@ -165,5 +139,4 @@ h1{
 p{
     font-size: 20px;
 }
-
 </style>
